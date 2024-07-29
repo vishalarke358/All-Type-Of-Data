33479c6e17940dfc16f064e0f7b20361d3182c9b
vishalarke358 <vishalarke358@gmail.com>
2024-07-29 15:46:35 +0530

    Test_04

---
+++ src/default_value/Thor.java
@@ -0,0 +1,11 @@
+package default_value;
+
+public class Thor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello, I am Vishal Arke");
+	}
+
+}
