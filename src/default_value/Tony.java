66e1a547c8ba99042949be8eec1d65f94693f18e
vishalarke358 <vishalarke358@gmail.com>
2024-07-29 14:43:23 +0530

    All Data Type

---
+++ src/default_value/Tony.java
@@ -0,0 +1,27 @@
+package default_value;
+
+
+class classP1{
+   void Run() {
+	
+	System.out.println( "Vehicle run");
+}
+
+}
+
+
+  public class Tony extends classP1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Tony obj=new Tony();
+		
+		obj.Run();
+		
+		
+		
+	}
+
+}
